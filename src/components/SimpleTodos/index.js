b9941bcae5a96385ee9610f8da722e2d46820f35
nikitha <nikithanikki474@gmail.com>
2024-12-13 19:52:18 +0000

    initial commit

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,144 @@
+import {useState} from 'react'
+import TodoItem from '../TodoItem'
+import './index.css'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+const SimpleTodos = () => {
+  const [todos, setTodos] = useState(initialTodosList)
+  const [title, setTitle] = useState('')
+  const [editedTodo, setEditedTodo] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+
+  const handleClickDelete = todoId => {
+    const newTodos = todos.filter(todo => todo.id !== todoId)
+    setTodos(newTodos)
+  }
+
+  const handleChange = event => {
+    setTitle(event.target.value)
+  }
+
+  const handleAddTodo = () => {
+    if (title !== '' && title !== null) {
+      let numOfTimes = 1
+      let num = ''
+      let t = ''
+      for (const ch of title) {
+        if (ch >= '0' && ch <= '9') {
+          num = num + ch
+        } else {
+          t = t + ch
+        }
+      }
+
+      if (num !== '') {
+        numOfTimes = Number(num)
+      }
+
+      for (let i = 0; i < numOfTimes; i++) {
+        setTodos(prevState => [
+          ...prevState,
+          {
+            id: Date.now() + i,
+            title: t,
+          },
+        ])
+      }
+      setTitle('')
+    }
+  }
+
+  const handleEditTodo = (todoId, todoTitle) => {
+    setNewTitle(todoTitle)
+    setEditedTodo(prevId => [...prevId, todoId])
+  }
+
+  const handleChangeEditTodo = event => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleSaveTodo = (id, todoTitle) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? {...todo, title: todoTitle} : todo,
+      ),
+    )
+    setEditedTodo(prevState => prevState.filter(todoId => todoId !== id))
+  }
+
+  return (
+    <div className="container">
+      <h1 className="heading">Simple Todos</h1>
+      <div className="add-todo-container">
+        <div>
+          <input
+            type="text"
+            value={title}
+            onChange={handleChange}
+            className="add-input"
+          />
+        </div>
+        <div>
+          <button type="button" onClick={handleAddTodo} className="btn add-btn">
+            Add
+          </button>
+        </div>
+      </div>
+      <ul className="todos-container">
+        {todos.length > 0 ? (
+          todos.map(todo => (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              handleClickDelete={handleClickDelete}
+              handleAddTodo={handleAddTodo}
+              handleEditTodo={handleEditTodo}
+              handleSaveTodo={handleSaveTodo}
+              editedTodo={editedTodo}
+              newTitle={newTitle}
+              handleChangeEditTodo={handleChangeEditTodo}
+            />
+          ))
+        ) : (
+          <p>Please add a todo!</p>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default SimpleTodos
---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,58 @@
+import './index.css'
+
+const TodoItem = ({
+  id,
+  title,
+  handleClickDelete,
+  handleEditTodo,
+  handleSaveTodo,
+  editedTodo,
+  newTitle,
+  handleChangeEditTodo,
+}) => (
+  <li className="todo-item">
+    {editedTodo === id ? (
+      <p>
+        <input type="text" value={newTitle} onChange={handleChangeEditTodo} />
+      </p>
+    ) : (
+      <p>{title}</p>
+    )}
+    <div className="btn-group">
+      <div>
+        {editedTodo === id ? (
+          <button
+            type="button"
+            className="btn save-btn"
+            onClick={() => {
+              handleSaveTodo(id, newTitle)
+            }}
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn edit-btn"
+            onClick={() => {
+              handleEditTodo(id, title)
+            }}
+          >
+            Edit
+          </button>
+        )}
+      </div>
+      <div>
+        <button
+          type="button"
+          className="btn delete-btn"
+          onClick={() => handleClickDelete(id)}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  </li>
+)
+
+export default TodoItem
